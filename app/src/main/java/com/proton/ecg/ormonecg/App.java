b507e50fac98dd367fed18a666f55dfed6d73832
yxf <50686181+yuxiongfeng@users.noreply.github.com>
2022-04-01 00:09:46 +0800

    Update App.java

--- app/src/main/java/com/proton/ecg/ormonecg/App.java
+++ app/src/main/java/com/proton/ecg/ormonecg/App.java
@@ -27,11 +27,7 @@ public class App extends Application {
                 .tag("BleLog--->")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
                 .build();
         Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog();     // 打印日志
-            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-        }
-        ARouter.init(this);
+    
         CarePatchECGCardKitManager.getInstance().init(this);
     }
 }
