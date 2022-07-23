692e9d8195840f5033b69dbe905c9ce63786c6d4
yxf <50686181+yuxiongfeng@users.noreply.github.com>
2022-07-23 20:12:03 +0800

    Update App.java

--- app/src/main/java/com/proton/ecg/ormonecg/App.java
+++ app/src/main/java/com/proton/ecg/ormonecg/App.java
@@ -2,12 +2,11 @@ package com.proton.ecg.ormonecg;
 
 import android.app.Application;
 
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.orhanobut.logger.AndroidLogAdapter;
-import com.orhanobut.logger.FormatStrategy;
-import com.orhanobut.logger.Logger;
-import com.orhanobut.logger.PrettyFormatStrategy;
 import com.proton.ecg.omron.CarePatchECGCardKitManager;
+import com.tencent.bugly.crashreport.CrashReport;
+import com.wms.logger.Logger;
+
+import java.io.File;
 
 /**
  * @Description:
@@ -20,14 +19,23 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
-                .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
-                .methodCount(0)         // (Optional) How many method line to show. Default 2
-                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
-                .tag("BleLog--->")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+        File loggerFile = getExternalFilesDir("logger");
+        if (!loggerFile.exists()) {
+            loggerFile.mkdirs();
+        }
+        com.wms.logger.Logger.newBuilder()
+                .tag("omronLog")
+                .showThreadInfo(false)
+                .methodCount(1)
+                .methodOffset(5)
+                .context(this)
+                .deleteOnLaunch(true)
+                .isDebug(BuildConfig.DEBUG)
+                .logPath(loggerFile.getPath())
+                .saveFile(BuildConfig.DEBUG)
                 .build();
-        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
-    
+        Logger.w("loggerFile path===", loggerFile.getPath());
+        CrashReport.initCrashReport(getApplicationContext(), "2fc46bc871", BuildConfig.DEBUG);
         CarePatchECGCardKitManager.getInstance().init(this);
     }
 }
